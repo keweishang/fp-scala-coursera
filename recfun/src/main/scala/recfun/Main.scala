6e21f29b8dc4f9c4b96f1b7cb51bc48525a146c9
Kewei Shang <kewei.shang@gmail.com>
2017-01-03 20:02:10 +0100

    feat: add pascal triangle solution

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,31 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      if (c > r) throw new IllegalArgumentException("col cannot be larger than row")
+      if (c == r || c == 0) 1
+      else pascal(c-1, r-1) + pascal(c, r-1)
+    }
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
