6874ba808d0d69a9b372ddda34457138c937a828
Kewei Shang <kewei.shang@gmail.com>
2017-01-04 01:17:37 +0100

    feat: add solution to Balance-parenthese and Count-change

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,21 +11,34 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-      if (c > r) throw new IllegalArgumentException("col cannot be larger than row")
-      if (c == r || c == 0) 1
-      else pascal(c-1, r-1) + pascal(c, r-1)
-    }
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c > r) throw new IllegalArgumentException("col cannot be larger than row")
+    if (c == r || c == 0) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def balance(acc: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) acc == 0
+      else if (acc < 0) false
+      else if (chars.head == '(') balance(acc + 1, chars.tail)
+      else if (chars.head == ')') balance(acc - 1, chars.tail)
+      else balance(acc, chars.tail)
+    }
+    balance(0, chars)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
+}
