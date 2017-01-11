2927f8008eb0b4ea82767e190d084a89e61e932b
Kewei Shang <kewei.shang@gmail.com>
2017-01-11 13:55:12 +0100

    feat: implement filter and union

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -36,23 +36,30 @@ abstract class TweetSet {
     * This method takes a predicate and returns a subset of all the elements
     * in the original set for which the predicate is true.
     *
-    * Question: Can we implment this method here, or should it remain abstract
+    * Question: Can we implement this method here, or should it remain abstract
     * and be implemented in the subclasses?
     */
-  def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
 
   /**
-    * This is a helper method for `filter` that propagetes the accumulated tweets.
+    * This is a helper method for `filter` that propagates the accumulated tweets.
     */
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet
 
   /**
     * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
     *
-    * Question: Should we implment this method here, or should it remain abstract
+    * Question: Should we implement this method here, or should it remain abstract
     * and be implemented in the subclasses?
     */
-  def union(that: TweetSet): TweetSet = ???
+  def union(that: TweetSet): TweetSet = {
+    def exists(tw: Tweet, set: TweetSet): Boolean = !set.contains(tw)
+
+    val left = filterAccUnion(exists, new Empty)
+    that.filterAccUnion(exists, left)
+  }
+
+  def filterAccUnion(p: (Tweet, TweetSet) => Boolean, acc: TweetSet): TweetSet
 
   /**
     * Returns the tweet from this set which has the greatest retweet count.
@@ -60,7 +67,7 @@ abstract class TweetSet {
     * Calling `mostRetweeted` on an empty set should throw an exception of
     * type `java.util.NoSuchElementException`.
     *
-    * Question: Should we implment this method here, or should it remain abstract
+    * Question: Should we implement this method here, or should it remain abstract
     * and be implemented in the subclasses?
     */
   def mostRetweeted: Tweet = ???
@@ -105,7 +112,10 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
+
+  def filterAccUnion(p: (Tweet, TweetSet) => Boolean, acc: TweetSet): TweetSet = acc
 
   /**
     * The following methods are already implemented
@@ -122,8 +132,17 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+    val children = right.filterAcc(p, left.filterAcc(p, acc))
+    if (p(elem)) children.incl(elem)
+    else children
+  }
 
+  def filterAccUnion(p: (Tweet, TweetSet) => Boolean, acc: TweetSet): TweetSet = {
+    val childrien = right.filterAccUnion(p, left.filterAccUnion(p, acc))
+    if (p(elem, acc)) childrien.incl(elem)
+    else childrien
+  }
 
   /**
     * The following methods are already implemented
